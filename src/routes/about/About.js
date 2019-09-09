582106c00c7691310e80c8862a80777d2702b3e5
Andrew Prince <drewnauts@gmail.com>
2019-09-09 19:43:14 -0400

    Added copy for About page

--- src/routes/about/About.js
+++ src/routes/about/About.js
@@ -5,8 +5,10 @@ export default function About() {
         <section className="about">
             <div>
                 <h2>What Is This?</h2>
-                <p>Ullamco proident est ipsum eu. Occaecat laboris id et enim commodo irure consectetur quis excepteur sunt aliquip est. Occaecat commodo aliquip nulla occaecat sint dolore sit. Amet laborum quis cupidatat sunt non ex culpa duis aliqua aliqua velit. Enim quis amet dolor esse laborum aute excepteur nisi esse sint ea in. Voluptate ad nisi laboris cillum Lorem sit ad sit nisi incididunt. Reprehenderit do est eiusmod enim sint duis elit aliquip officia.</p>
-                <p>Irure sit Lorem anim occaecat id irure voluptate voluptate anim deserunt cupidatat amet ullamco. Adipisicing fugiat labore nostrud nisi laborum dolor velit elit magna exercitation laborum adipisicing minim duis. Mollit consequat eu enim nisi laborum aliquip sint et.</p>
+                <p>Over time, even a passively managed portfolio will deviate from its intended target allocation due to the constant fluctuation of the market. Rebalancing is the process of realigning the weightings of a portfolio of assets. Rebalancing involves periodically buying or selling assets in a portfolio to maintain an original or desired level of asset allocation or risk. </p>
+                <p>NetBalance aims to expedite that process with ease by automatically calculating the steps needed to keep your portfolio in balance. This tool offers an easy way to group and categorize holdings, set target allocations and fetch current stock prices for calculation.</p>
+                <p>To learn more about rebalancing, visit <a href="https://www.investopedia.com/terms/r/rebalancing.asp" target="_blank">Investopedia</a>.</p>
+                <p>For trusted Canadian model portfolios (with varying degrees of asset mixes and risk), I recommend <a href="https://canadiancouchpotato.com/model-portfolios/" target="_blank">Canadian Couch Potato</a>.</p>
             </div>
 
             <div>
@@ -17,12 +19,19 @@ export default function About() {
 
             <div>
                 <h2>Source Code</h2>
-                <p>Irure sit Lorem anim occaecat id irure voluptate voluptate anim deserunt cupidatat amet ullamco. Adipisicing fugiat labore nostrud nisi laborum dolor velit elit magna exercitation laborum adipisicing minim duis. Mollit consequat eu enim nisi laborum aliquip sint et.</p>
+                <p>You can view the source code here: <a href="https://github.com/aprincedev/netbalance" target="_blank">https://github.com/aprincedev/netbalance</a></p>
+                <p>Feel free to fork this project and propose features/revisions. Read the <a href="https://github.com/aprincedev/netbalance/blob/master/README.md" target="_blank">README</a> for more information regarding pull requests and development information.</p>
             </div>
 
             <div>
                 <h2>Contact/Help</h2>
-                <p>Irure sit Lorem anim occaecat id irure voluptate voluptate anim deserunt cupidatat amet ullamco. Adipisicing fugiat labore nostrud nisi laborum dolor velit elit magna exercitation laborum adipisicing minim duis. Mollit consequat eu enim nisi laborum aliquip sint et.</p>
+                <p>Please contact the author, <b>Andrew Prince</b>, with any questions, comments, or feedback regarding this project.</p>
+                <ul>
+                    <li>Email: <a href="mailto:hello@aprincedev.com">hello@aprincedev.com</a></li>
+                    <li>Website: <a href="https://www.aprincedev.com" target="_blank">https://aprincedev.com</a></li>
+                    <li>Twitter: <a href="https://www.twitter.com/aprincedev" target="_blank">@aprincedev</a></li>
+                    <li>GitHub: <a href="https://github.com/aprincedev" target="_blank">https://github.com/aprincedev</a></li>
+                </ul>
             </div>
 
         </section>
